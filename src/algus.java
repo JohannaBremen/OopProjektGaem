5897d29b754c62301dd9677f5ffa168124fb59f6
Johanna Angela Bremen <johanna.bremen@gmail.com>
2019-04-03 17:25:53 +0300

    loogika fix

--- src/algus.java
+++ src/algus.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class algus {
     public static void nullpunkt(){
         System.out.println("Tere, see on mängu algus.");
-        System.out.println("Kas sa oled varem mänginud? (Trüki 'Y', kui oled ning 'N', kui ei ole ja vajuta klahvi ENTER)");
+        System.out.println("Kas sa oled varem mänginud? (Y/N) (Trüki 'N', kui ei ole ja vajuta klahvi ENTER)");
         Scanner variandid = new Scanner(System.in);
         String vastus = variandid.nextLine();
         if(vastus.equals("Y")){
