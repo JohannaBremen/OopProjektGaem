2532927869b251d4c2769c305e722a014f6ecc96
Hain Zuppur <h.zuppur@gmail.com>
2019-04-04 10:15:05 +0300

    Lisasin storit

--- src/Vangla.java
+++ src/Vangla.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Vangla {
     static void kong(){
         System.out.println("Suht kurb olemine siin kongis, ainult üks kongikaaslane.\n" +
                 "Tal on suur arm põse peal ja näol kahtlane naeratus\n" +
-                "Kas sa tahad temaga rääkida? (Y/N)");
+                " Kas sa tahad temaga rääkida? (Y/N)");
+        Scanner variandid = new Scanner(System.in);
+        String vastus = variandid.nextLine();
+        if(vastus.equals("Y")){
+            System.out.println("Vang vastab rõõmsalt, et tema ei pea siin külmas kongis enam kaua passima,\n" +
+                    " sest saab homme vabaks");
+        }else{
+            System.out.println();
+        }
     }
 }
